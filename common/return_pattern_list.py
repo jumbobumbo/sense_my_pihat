cc643bf99ef5cb98d4d24a2c69188688872ca5c3
jumbobumbo <dwheels91@gmail.com>
2020-03-18 21:07:45 +0000

    i dont understand why this isn't working

--- common/return_pattern_list.py
+++ common/return_pattern_list.py
@@ -5,6 +5,7 @@ import json
 class PatternList:
     def __init__(self, json_file: str):
         self.pattern_json = Path('patterns/', json_file)
+        self.rgb = {"red": 0, "green": 1, "blue": 2}
 
     @property
     def pattern_json(self) -> dict:
@@ -15,4 +16,25 @@ class PatternList:
         with open(json_file, "r") as f:
             self.__pattern_json = json.load(f)
 
+    def _create_base_list(self):
+        """
+        creates list of 64 elements from the "base" key in pattern_json
+        the key is then deleted from the dict
+        :return: base list
+        """
+        base_list = [self.pattern_json["base"]] * 64
+        del self.pattern_json["base"]
+        return base_list
+
+    def create_pattern_list(self):
+        """
+        creates RGB list for pattern
+        :return: list
+        """
+        return_list = self._create_base_list()
+        for colour, values in self.pattern_json.items():
+            for intensity, idx in values.items():
+                for i in idx:
+                    return_list[i][self.rgb["colour"]] = int(intensity)
+        return return_list
 
